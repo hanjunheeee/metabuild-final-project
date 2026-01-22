82e6f0f608bee78a7377f13534c518b028aa0b60
junhee han <1702o505@naver.com>
2026-01-22 11:35:08 +0900

    260122 알라딘 정상화

--- backend/src/main/java/com/example/ex02/Book/service/BookUpsertService.java
+++ backend/src/main/java/com/example/ex02/Book/service/BookUpsertService.java
@@ -17,20 +17,16 @@ public class BookUpsertService {
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void saveIfAbsent(BookEntity book) {
+        // MySQL 호환: INSERT IGNORE (isbn이 중복이면 무시)
         entityManager.createNativeQuery("""
-            MERGE INTO book b
-            USING (SELECT ? AS isbn FROM dual) src
-            ON (b.isbn = src.isbn)
-            WHEN NOT MATCHED THEN
-              INSERT (isbn, title, author, publisher, image_url)
-              VALUES (?, ?, ?, ?, ?)
+            INSERT IGNORE INTO book (isbn, title, author, publisher, image_url)
+            VALUES (?, ?, ?, ?, ?)
         """)
             .setParameter(1, book.getIsbn())
-            .setParameter(2, book.getIsbn())
-            .setParameter(3, book.getTitle())
-            .setParameter(4, book.getAuthor())
-            .setParameter(5, book.getPublisher())
-            .setParameter(6, book.getImageUrl())
+            .setParameter(2, book.getTitle())
+            .setParameter(3, book.getAuthor())
+            .setParameter(4, book.getPublisher())
+            .setParameter(5, book.getImageUrl())
             .executeUpdate();
     }
 }
